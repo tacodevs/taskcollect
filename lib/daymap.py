9b033ca0209b0e2f43dfa7301780de14d934e8ee
Wasabi1092 <62374573+Wasabi1092@users.noreply.github.com>
2021-09-12 16:53:50 +0930

    lessons functionality
    completed it for 2 days, unfortunately cannot request date to pull lessons from yet

--- lib/daymap.py
+++ lib/daymap.py
@@ -94,9 +94,201 @@ def daymap_get(webpage, username, password):
     return s4.text
 
 # Function to get the timetable for a certain day.
-def get_timetable(date, username, password):
+# Function to get the timetable for a couple of days
+def get_lessons(username, password):
+
+    #define lists and dictionaries: 2 sets, one for each day
     timetable = {}
-    return timetable
+    tomorrow_timetable = {}
+    lesson_list = []
+    lesson_list2 = []
+
+    #get html from daymap
+    page_html = daymap_get("https://daymap.gihs.sa.edu.au/daymap/student/dayplan.aspx", username, password)
+
+    #split lines
+    page_html = page_html.split("\n")
+
+    #take specific line to work with
+    lesson_line = page_html[211]
+
+    #empties list to optimise
+    page_html = []
+    
+    #looks for week class
+    class_index = lesson_line.find("diaryWeek")
+
+    #cuts it down to minimise searching
+    lesson_line = lesson_line[class_index+10:-1]
+    count = 0
+
+    #finds the end of the related tag
+    for char in lesson_line:
+        count += 1
+        if char == ">":
+            break
+
+    #cuts down again to minimise searching for next step
+    lesson_line = lesson_line[count:-1]
+
+    #sets variable for week
+    week = ""
+    for char in lesson_line:
+
+        #loops until the start of the next tag i.e. when the text stops
+        if char == "<":
+            break
+        week = week + char
+    
+    #today var is set
+    today = ""
+
+    #looks for the day class
+    class_index = lesson_line.find("diaryDay")
+
+    #cuts down to minimise searching
+    lesson_line = lesson_line[class_index+9:-1]
+    count = 0
+
+    #finds the end of the related tag
+    for char in lesson_line:
+        count += 1
+        if char == ">":
+            break
+
+    #cuts down again to minimise searching
+    lesson_line = lesson_line[count:-1]
+    
+    #saves the text between the opening and closing tag
+    for char in lesson_line:
+        if char == "<":
+            break
+        today = today + char  
+    
+    #checks the day and tells it how many lessons there are on that day to look for
+    if "Wednesday" in today:
+        limit = 4
+    elif ("Friday") in today:
+        limit = 6
+    else:
+        limit = 5
+    Daycount = 1
+
+    #loop to find the lesson time and subject
+    while Daycount <= limit:
+
+        #lesson time class finding
+        time_index = lesson_line.index("class='t'")
+        count = 0
+
+        #cuts down to minimise searching
+        lesson_line = lesson_line[time_index+9:]
+
+        #finds the end of the opening tag
+        for char in lesson_line:
+            count +=1
+            if char == ">":
+                break
+        
+        #cuts down to minimise searching
+        lesson_line = lesson_line[count:]
+        time = ""
+
+        #takes the text between the tags for the time
+        for char in lesson_line:
+            
+            if char == "<":
+                break
+            time = time + str(char)
+        
+        #finds the subject class
+        subject_index = lesson_line.find("event")
+        count = 0
+
+        #cuts down to minimise searching
+        lesson_line = lesson_line[subject_index+6:]
+
+        #finds the end of the tag
+        for char in lesson_line:
+            count += 1
+            if char == ">":
+                break
+        
+        #cuts down to minimise searching
+        lesson_line = lesson_line[count:]
+        subject = ""
+
+        #notes the subject name
+        for char in lesson_line:
+            if char == "<":
+                break
+            subject = subject + str(char)
+
+        #adds it to the list of subjects
+        lesson_list.append(subject[:-2])
+
+        #adds it to a dictionary along with the date and time
+        timetable[subject[:-2]] = [today, time]
+        Daycount += 1
+        #if statement to check if the next school day is monday
+    class_index = lesson_line.find("diaryDay")
+    lesson_line = lesson_line[class_index+10:]
+    count = 0
+
+    #basically the same as above
+    for char in lesson_line:
+        count += 1
+        if char == ">":
+            break
+    lesson_line = lesson_line[count:]
+    tomorrow = ""
+
+    #basically the same as above
+    for char in lesson_line:
+        if char == "<":
+            break
+        tomorrow = tomorrow + char
+
+    #same code from above repeated, only difference is saving to a different set of variables to minimise confusion
+    if "Wednesday" in tomorrow:
+        limit = 4
+    elif ("Friday") in tomorrow:
+        limit = 6
+    else:
+        limit = 5
+    Daycount = 1
+    while Daycount <= limit:
+        time_index = lesson_line.index("class='t'")
+        count = 0
+        lesson_line = lesson_line[time_index+9:]
+        for char in lesson_line:
+            count +=1
+            if char == ">":
+                break
+        lesson_line = lesson_line[count:]
+        time = ""
+        for char in lesson_line:
+            
+            if char == "<":
+                break
+            time = time + str(char)
+        subject_index = lesson_line.find("event")
+        count = 0
+        lesson_line = lesson_line[subject_index+6:]
+        for char in lesson_line:
+            count += 1
+            if char == ">":
+                break
+        lesson_line = lesson_line[count:]
+        subject = ""
+        for char in lesson_line:
+            if char == "<":
+                break
+            subject = subject + str(char)
+        lesson_list2.append(subject[:-2])
+        tomorrow_timetable[subject[:-2]] = [tomorrow, time]
+        Daycount += 1
+    return week, today, timetable, lesson_list, tomorrow_timetable, lesson_list2
 
 # Function to get the specified user's DayMap messages.
 def get_msgs(username, password):
