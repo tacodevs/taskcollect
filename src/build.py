2b4b6e9f4fb2f595096dffc956862e003f4ebb51
Ray <rayokamoto@noreply.codeberg.org>
2022-10-15 22:23:04 +1030

    Improved the build script
    - Added an option to compile for all available OSs and architectures
    - Added the option to compile for select OSs and architectures
    - Added a help command

--- src/build.py
+++ src/build.py
@@ -1,47 +1,89 @@
 #!/usr/bin/python3
 
-from os import *
-import sys
+import os
+#import platform
 import subprocess
+import sys
 
-platforms = """
-	386/linux
-	386/windows
-	amd64/darwin
-	amd64/linux
-	amd64/windows
-	arm/linux
-	arm64/darwin
-	arm64/linux
-"""
 
-for platform in platforms.split():
-	platform = platform.split("/")
-	arch = platform[0]
-	os = platform[1]
+platforms = {
+    "darwin": ["amd64", "arm64"],
+    "linux": ["386", "amd64", "arm", "arm64"],
+    "windows": ["386", "amd64"]
+}
 
-	ext = ""
-	if os == "windows":
-		ext = ".exe"
+def build(op_sys: str, arch: str):
+    ext = ""
+    if op_sys == "windows":
+        ext = ".exe"
 
-	cmd = [
-		"go", "build", "-ldflags=-s -w",
-		"-o", f"../prg/{arch}/{os}/taskcollect{ext}", ".",
-	]
+    output = f"../prg/{op_sys}/{arch}/taskcollect{ext}"
+    source = "."
+    cmd = [
+        "go", "build", "-ldflags=-s -w",
+        "-o", output, source
+    ]
 
-	env = environ
-	env["CGO_ENABLED"] = "0"
-	env["GOARCH"] = arch
-	env["GOOS"] = os
+    env = os.environ
+    env["CGO_ENABLED"] = "0"
+    env["GOARCH"] = arch
+    env["GOOS"] = op_sys
 
-	try:
-		subprocess.run(
-			cmd,
-			stdin=sys.stdin,
-			stdout=sys.stdout,
-			stderr=sys.stderr,
-			check=True,
-			env=env,
-		)
-	except subprocess.CalledProcessError:
-		sys.exit(1)
+    try:
+        print(f"Compiling taskcollect for {op_sys} on {arch}... ", end="", flush=True)
+        subprocess.run(
+            cmd,
+            stdin=sys.stdin,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+            check=True,
+            env=env,
+        )
+        print("Done")
+    except subprocess.CalledProcessError as e:
+        print(e)
+        sys.exit(1)
+
+def run(argv: list[str]):
+    argv = argv[1:]
+    if argv[0] == "all":
+        for op_sys, archs in platforms.items():
+            for arch in archs:
+                build(op_sys, arch)
+    else:
+        for i in argv:
+            # TODO: Add error handling?
+            i = i.split("/")
+            build(i[0], i[1])
+
+def main(argc: int, argv: list[str]):
+    if argc == 1:
+        print("No arguments provided. See 'help' for more information")
+    elif argv[1] == "help":
+        print(
+            "--- taskcollect: Build help ---\n\n"
+            "Supported OS and architectures:\n"
+            "- darwin (amd64, arm64)\n"
+            "- linux (386, amd64, arm, arm64)\n"
+            "- windows (386, arm64)\n"
+            "\n"
+            "USAGE:\n"
+            "    [<<OS>/<ARCH>> ...]\n" # - Provide a valid combination of OS and architecture. Several can be built at once
+            "\n"
+            "COMMANDS:\n"
+            "    all - Build for all platforms (this may take a while)\n"
+            "    help - Shows this command\n"
+        )
+    elif argv[1] == "all":
+        run(argv)
+    elif argc >= 1:
+        run(argv)
+    else:
+        print("Invalid argument")
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv)
+    main(argc, argv)
+    #print(platform.uname())
