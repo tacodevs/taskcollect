7f9e0b4224bea3f96bcd6234a15cde581afb448f
kvo <kvo@envs.net>
2023-01-08 12:11:42 +1030

    Require username and passwords to be not empty
    Previously username and passwords could be empty which allowed direct access to TaskCollect.
    Although not a security issue, it was a UI/UX issue that needed to be resolved.
    Username and passwords cannot be empty anymore, and this is mandated both by the server and standards-compliant clients.

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 0.1.0 (build c6865ec8d68cc5c1f123d8d3e84d08a2d66eddb7)"
+const tcVersion = "TaskCollect 0.1.0 (build ac89e3d3aa1f07b90afa8634e04c54688d343442)"
