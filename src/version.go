8fd425427e2edd3e93194aa3ab2ed73993def6e3
Ray Okamoto <git@rayokamoto.com>
2023-01-12 21:05:08 +1030

    docs: Update README

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 0.1.0 (build 6d00f7ee07925c78f39379372a4e76b11cb65b0b)"
+const tcVersion = "TaskCollect 0.1.0 (build 17b82500e2355e0d65c9a35f4813105365f222de)"
