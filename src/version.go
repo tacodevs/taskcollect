00adc14e3bb05300e0ee3d49413d7b219ae26c76
kvo <kvo@envs.net>
2023-01-13 22:21:53 +1030

    Fix graded task fetching issues
    Memory is now allocated correctly for concurrent graded task information retrieval.
    Pointers are now effectively used for memory sharing in place of their erroneous omission.

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 0.1.0 (build 7591d42f3a83c90e68936434dad9e4d76eb3e225)"
+const tcVersion = "TaskCollect 0.1.0 (build 90215736a11b0a4ee908127de7bfd63727599c0c)"
