e67e763c42f4db41a3c8ac49b18dac2aa9407b4c
kvo <kvo@envs.net>
2023-02-15 22:12:47 +1030

    Make due date on individual task pages more visible

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.3 (build fbfc86bf0fc60f7d92fa043655f8b9ed5da94275)"
+const tcVersion = "TaskCollect 1.0.3 (build b7f2fad120bb5e773c83edc151ee6af7e6049212)"
