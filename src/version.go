ff94ef75e1f217601149cbadf4fb2d070437480e
kvo <kvo@envs.net>
2023-02-23 20:30:13 +1030

    Improve timetable experience on mobile
    On mobile, all days before the current day are not displayed.
    This is to ensure the day at the top of the screen is the current day, for easier navigation.
    The top margin for each lesson block has been increased to the size of the bottom margin for aesthetic appeal.

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.4 (build 0a2a6a1e3c6816e608d6a0fd14db06eca2008d93)"
+const tcVersion = "TaskCollect 1.0.4 (build 867078e25b0faa06e21c6997bc8b0cfe0fc78d98)"
