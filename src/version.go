fd4490da9e231209f06e499f6671ae7caab4bc2d
kvo <kvo@envs.net>
2023-03-02 18:32:36 +1030

    Add privacy policy and ToS links to login page

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.4 (build 4c9acad812c51905d43dbf60be55bc65b205bb1b)"
+const tcVersion = "TaskCollect 1.0.4 (build 2e8ca6ead7e944cf8dae7e2a6dd89ddaea0cc8c2)"
