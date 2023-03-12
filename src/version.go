f14a99a1a8fc3102f8f8be380caa73dd732d5978
kvo <kvo@envs.net>
2023-03-12 22:50:34 +1030

    Allow mixed-case "CURRIC" username prefixes for GIHS users

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.4 (build 27cbe4659027c3c32aa2cca5e7b91b46751771fe)"
+const tcVersion = "TaskCollect 1.0.4 (build d1895c0b9e6cfff5b60fa14f75fc27367d4f55bd)"
