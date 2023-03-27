da98eacf9bff740d3142ce998e2d1362cbcc4086
kvo <kvo@envs.net>
2023-03-27 17:45:53 +1030

    Use time.ParseInLocation() for when Daymap uses RFC3339 time formats

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.5 (build 54ec3854f0a6268a524b7e8b83a647899efccb86)"
+const tcVersion = "TaskCollect 1.0.5 (build c48ca961cae3fa7bdb1517fcce2e37cdc277e7f8)"
