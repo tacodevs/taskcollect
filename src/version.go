16af97dafe2971ec9a5a0fd1dd8febfef0c4e231
kvo <kvo@envs.net>
2023-04-03 20:20:23 +0930

    Ensure lessons are in correct order in mobile view

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.5 (build c48ca961cae3fa7bdb1517fcce2e37cdc277e7f8)"
+const tcVersion = "TaskCollect 1.0.5 (build abe0368dfb9ff5178bfd3ff15a614561b367d40e)"
