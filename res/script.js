5d6923d07fa9fc49a5ce5063c26401ce1dad0625
al2f <al2f@noreply.codeberg.org>
2023-04-24 10:46:59 +0930

    Began work on loading animation

--- res/script.js
+++ res/script.js
@@ -2,32 +2,10 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle("open");
 }
 
-//Replace the whole page body but the tabs with the loading animation
 document.addEventListener('click', function (event) {
 	
-	var loaderBody = `<div class="loader-container">
-<div class="loader" scale=1></div>
-<div>`
-	var loaderStyle = `<style>
-.loader-container {
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-}
-</style>`
-	if (event.target.tagName == "A") {
-		//Select everything that isn't the navigation and remove it
-		var divs = document.querySelectorAll("body > *:not(nav)");
-		for(i=0;i<divs.length;i++) {
-		  divs[i].remove()
-		}
-		
-		// Add the loader div the body of the HTML that's left
-		document.body.innerHTML += loaderBody
-
-		// Add the loader style to the head of the HTML
-		document.head.innerHTML += loaderStyle
+	if (event.target.tagName) == "A" {
+		//Replace everything but the top tabs with the loading circle located at res/loading.html
 	}
 
 }, false);
