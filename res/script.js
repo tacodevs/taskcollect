3bfe53317f91bec84e3f6f62f9892bb21ae2bca5
al2f <al2f@noreply.codeberg.org>
2023-04-24 10:46:59 +0930

    Began work on loading animation

--- res/script.js
+++ res/script.js
@@ -1,3 +1,11 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle("open");
 }
+
+document.addEventListener('click', function (event) {
+	
+	if (event.target.tagName) == "A" {
+		//Replace everything but the top tabs with the loading circle located at res/loading.html
+	}
+
+}, false);
