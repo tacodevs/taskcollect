f0e4357389ec5a669df94ef06bc9d691454b2d8a
kvo <kvo@envs.net>
2023-04-25 00:22:44 +0930

    Add auth, events, and messages multiplexing to package plat

--- src/plat/types.go
+++ src/plat/types.go
@@ -1,26 +1,80 @@
 package plat
 
 import (
+	"image/color"
 	"io"
 	"time"
 )
-
-// User represents an authenticated TaskCollect user.
-type User struct {
-	Timezone   *time.Location
-	School     string
-	DispName   string
-	Username   string
-	Password   string
-	Token      string
-	SiteTokens map[string]string
-	GAuthID    []byte
-}
 
 // Class represents a class into which the user is enrolled in.
 type Class struct {
 	Name     string
 	Link     string
+	Platform string
+	Id       string
+}
+
+// Event represents a calendar event.
+type Event struct {
+	Name     string
+	Start    time.Time
+	End      time.Time
+	Location string
+	Category string
+	Color    color.Color
+	Platform string
+}
+
+// File represents a file submission to a platform.
+type File struct {
+	Name     string
+	MimeType string
+	Reader   io.Reader
+}
+
+// Grade represents a grade for a class in a report card.
+type Grade struct {
+	Class string
+	Grade string
+	Score float64
+}
+
+// Item represents a generic task/resource item.
+type Item Task
+
+// Lesson represents a lesson.
+type Lesson struct {
+	Start   time.Time
+	End     time.Time
+	Class   string
+	Room    string
+	Teacher string
+	Notice  string
+}
+
+// Message represents a parsed email-like message of a proprietary format.
+type Message struct {
+	From    string
+	To      string
+	Sent    time.Time
+	Subject string
+}
+
+// Report represents a report card.
+type Report struct {
+	Grades   []Grade
+	Released time.Time
+}
+
+// Resource represents an educational resource provided by a teacher for a
+// class.
+type Resource struct {
+	Name     string
+	Class    string
+	Link     string
+	Desc     string
+	Posted   time.Time
+	ResLinks [][2]string
 	Platform string
 	Id       string
 }
@@ -45,45 +99,16 @@ type Task struct {
 	Id        string
 }
 
-// File represents a file submission to a platform.
-type File struct {
-	Name     string
-	MimeType string
-	Reader   io.Reader
-}
-
-// Grade represents a grade for a class in a report card.
-type Grade struct {
-	Class string
-	Grade string
-	Score float64
-}
-
-// Lesson represents a lesson.
-type Lesson struct {
-	Start   time.Time
-	End     time.Time
-	Class   string
-	Room    string
-	Teacher string
-	Notice  string
-}
-
-// Report represents a report card.
-type Report struct {
-	Grades   []Grade
-	Released time.Time
-}
-
-// Resource represents an educational resource provided by a teacher for a
-// class.
-type Resource struct {
-	Name     string
-	Class    string
-	Link     string
-	Desc     string
-	Posted   time.Time
-	ResLinks [][2]string
-	Platform string
-	Id       string
+// User represents an authenticated TaskCollect user.
+type User struct {
+	Timezone   *time.Location
+	School     string
+	DispName   string
+	Email      string
+	Username   string
+	Password   string
+	Token      string
+	SiteTokens map[string]string
+	// TODO: Move this elsewhere.
+	GAuthID    []byte
 }
