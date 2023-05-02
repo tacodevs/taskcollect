c8eece896f76fdc3925dd10490d7da5f55980bf9
kvo <kvo@envs.net>
2023-05-02 12:13:00 +0930

    Replace index operation with std.Access in Daymap-fetching code

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.5 (build 82b2f7fe7246f71fe4739eb82aaeefc91863504a)"
+const tcVersion = "TaskCollect 1.0.5 (build 0f38c54d8805a11d2e6688dad4cccee79bf9b459)"
