88016a5bc06499954f2e2bda34e09e310fa2ee71
kvo <kvo@envs.net>
2023-05-04 12:32:47 +0930

    Display lesson duration in HTML timetable

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.5 (build 139bc810c56b13f329feccb199b67daaa0b17788)"
+const tcVersion = "TaskCollect 1.0.5 (build dc7db8c30d0a3575209a3a865c3126a826048ebf)"
