35d4b05d2a8a6ba004cf597e3bf9b989572ee725
kvo <kvo@envs.net>
2023-05-04 12:54:35 +0930

    Display lesson duration in image timetable

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.5 (build dc7db8c30d0a3575209a3a865c3126a826048ebf)"
+const tcVersion = "TaskCollect 1.0.5 (build a423d1cb1717545693fff1da6cc1ba5cb5234a48)"
