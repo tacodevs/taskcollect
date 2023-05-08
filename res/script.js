4ef7a1b15aac01f1327a29a607be565fb93bfb87
al2f <al2f@noreply.codeberg.org>
2023-05-09 07:24:50 +0930

    Moved loader container styles to scss file. Event listener handles buttons as well.

--- res/script.js
+++ res/script.js
@@ -8,26 +8,14 @@ document.addEventListener('click', function (event) {
 	var loaderBody = `<div class="loader-container">
 <div class="loader" scale=1></div>
 <div>`
-	var loaderStyle = `<style>
-.loader-container {
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-}
-</style>`
-	if (event.target.tagName == "A") {
+	if (["A", "BUTTON"].includes(event.target.tagName)) {
 		//Select everything that isn't the navigation and remove it
 		var divs = document.querySelectorAll("body > *:not(nav)");
 		for(i=0;i<divs.length;i++) {
 		  divs[i].remove()
 		}
-		
 		// Add the loader div the body of the HTML that's left
 		document.body.innerHTML += loaderBody
-
-		// Add the loader style to the head of the HTML
-		document.head.innerHTML += loaderStyle
 	}
 
 }, false);
