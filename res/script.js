d66f49ef2c64111aa658f938ef5023a45a0dfce6
al2f <al2f@noreply.codeberg.org>
2023-05-08 22:12:56 +0930

    A parent div element is added to center the loading animation

--- res/script.js
+++ res/script.js
@@ -5,7 +5,17 @@ function toggleMobileMenu(menu) {
 //Replace the whole page body but the tabs with the loading animation
 document.addEventListener('click', function (event) {
 	
-	var loaderBody = `<div class="loader" scale="1"></div>`
+	var loaderBody = `<div class="loader-container">
+<div class="loader" scale=1></div>
+<div>`
+	var loaderStyle = `<style>
+.loader-container {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+}
+</style>`
 	if (event.target.tagName == "A") {
 		//Select everything that isn't the navigation and remove it
 		var divs = document.querySelectorAll("body > *:not(nav)");
@@ -15,6 +25,9 @@ document.addEventListener('click', function (event) {
 		
 		// Add the loader div the body of the HTML that's left
 		document.body.innerHTML += loaderBody
+
+		// Add the loader style to the head of the HTML
+		document.head.innerHTML += loaderStyle
 	}
 
 }, false);
