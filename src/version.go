a067bbac5eab98cf90e7e101964dbbb741971d69
kvo <kvo@envs.net>
2023-05-28 15:30:14 +0930

    Remove unnecessary spacing in "src/plat/daymap/misc.go"

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = "TaskCollect 1.0.6 (build a263a5e3e77789863f383530fd65ab3f13d89aeb)"
+const tcVersion = "TaskCollect 1.0.6 (build 64395641d07d92c92064266e84caf8382ed1f400)"
