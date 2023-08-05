21d42b54b00d5f18f5ca6d38c1500bb93d9afcd9
kvo <kvo@envs.net>
2023-08-05 21:52:29 +0930

    Omit Daymap calendar events from lessons struct

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const tcVersion = `TaskCollect 1.0.6 (build 6b215e85b8aa6422774d8ea21bdc0ba90cf143d0), branch "google-api"`
+const tcVersion = `TaskCollect 1.0.6 (build 4a6e4aff40ea909a3344016fcc8a2c3fc4393ff8), branch "google-api"`
