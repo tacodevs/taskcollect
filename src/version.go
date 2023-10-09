876611e25cb7ffcf5b7103a6528a5dff83908c45
kvo <kvo@envs.net>
2023-10-10 08:28:18 +1030

    Update dependencies

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const version = "TaskCollect 1.0.6 (build 116cfc31613bbf3d9282486452d7ad819f71fbe0)"
+const version = "TaskCollect 1.0.6 (build a676e36ef687f84c6c2808028b253b9e874ebf54)"
