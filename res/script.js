3248c8da286798ea49af69c0f3636ccdcf6769a6
kvo <kvo@envs.net>
2024-04-17 18:07:44 +0930

    res: fix code style in res/script.js
    Removed console.log statements left over from debugging. Made code
    more concise through the knowledge of operator precedence rules.
    Formatted the JS script to use consistent 2-space indentation,
    consistent spacing between keywords and braces, and declarations of
    local variables with "let". The loaderBody global variable is now
    isolated at the top of the script file. In addition, variables such as
    first_parent and second_parent have been renamed to parent,
    grandparent, etc.

--- res/script.js
+++ res/script.js
@@ -1,57 +1,46 @@
+var loaderBody = `<div class="loader-container">
+<div class="loader" scale=1></div>
+<div>`
+
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
 
-function applyLoader(){
-	var loaderBody = `<div class="loader-container">
-<div class="loader" scale=1></div>
-<div>`
-
-	// Select everything that isn't the navigation bar and remove it.
-    var divs = document.querySelectorAll("body > *:not(nav)");
-    for (i = 0; i < divs.length; i++) {
-      divs[i].remove();
-    }
-    // Add the loader div the body of the HTML that's left.
-    document.body.innerHTML += loaderBody;
+function applyLoader() {
+  var divs = document.querySelectorAll("body > *:not(header)");
+  for (i = 0; i < divs.length; i++) {
+    divs[i].remove();
+  }
+  document.body.innerHTML += loaderBody;
 }
 
-// Replace the whole page body but the tabs with the loading animation.
-document.addEventListener('click', function (event) {
-  
+// Replace the whole page body but the navbar with the loading animation.
+document.addEventListener("click", function (event) {
+  // Skip middle clicks and ctrl+clicks
+  if (event.ctrlKey || event.which == 2) {
+    return;
+  }
+
   if (["A", "BUTTON"].includes(event.target.tagName)) {
-	
-	// Skip middle clicks and ctrl+clicks	
-	if (event.ctrlKey || event.which == 2) return;
-	
-	// Show loading animation when clicking on links in navigation	
-	var navs = document.getElementsByTagName("nav")
-	console.log(navs)
-	if (navs != null){
-		var nav = navs[0]
-		console.log(nav)
-		if ( nav != null ){
-			if (nav.contains(event.target)){
-				applyLoader()
-			}
-		}
-	}
-	
-	// Show loading animation when cliking on resource/task links
-	var first_parent = event.target.parentElement
-	console.log(first_parent.tagName)
-	if(first_parent != null){
-		if(first_parent.tagName == "P"){
-			var second_parent = first_parent.parentElement
-			if(second_parent != null){
-				if(second_parent.tagName == "DIV"){
-					var third_parent = second_parent.parentElement
-					if(third_parent.tagName == "DETAILS"){
-						applyLoader()
-					}
-				}
-			}
-		}
-	}
+    // Show loading animation when clicking on links in navigation	
+    let navs = document.getElementsByTagName("nav");
+    if (navs != null) {
+      let nav = navs[0];
+      if (nav != null && nav.contains(event.target)) {
+        applyLoader();
+      }
+    }
+
+    // Show loading animation when cliking on resource/task links
+    let parent = event.target.parentElement;
+    if (parent != null && parent.tagName == "P") {
+      let grandparent = parent.parentElement;
+      if (grandparent != null && grandparent.tagName == "DIV") {
+        let greatGrandparent = grandparent.parentElement;
+        if (greatGrandparent.tagName == "DETAILS") {
+          applyLoader();
+        }
+      }
+    }
   }
 }, false);
