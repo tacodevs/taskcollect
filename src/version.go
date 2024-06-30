ea23d0a3b2de93036d7bcf281d7de18e0c7fe73d
kvo <kvo@envs.net>
2024-06-30 16:08:32 +0930

    src/server/auth.go: use true random number generator
    Signed-off-by: kvo <kvo@envs.net>

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const version = "TaskCollect v1.0.6 (build fe7a33ac0b4724011bdabe1d445ff56611d93a3d)"
+const version = "TaskCollect v1.0.6 (build 08733839dc24b71299a45baeb48c51598b8b557e)"
