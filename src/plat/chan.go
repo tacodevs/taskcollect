b174aadf331b0aea12b27c345063b4cf4529d9e9
kvo <kvo@envs.net>
2024-07-05 22:06:51 +0930

    src/plat/chan.go: add improved concurrency helpers
    Signed-off-by: kvo <kvo@envs.net>

--- src/plat/chan.go
+++ src/plat/chan.go
@@ -11,3 +11,15 @@ func Mark[T any](done *int, c chan T) {
 		close(c)
 	}
 }
+
+func Send[T any](c chan T, v T) {
+	c <- v
+}
+
+func Wait[T any](c chan T, done chan bool, jobs int) {
+	for i := 0; i < jobs; i++ {
+		<-done
+	}
+	close(done)
+	close(c)
+}
--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const version = "TaskCollect v1.0.6 (build 4aeb154ae5cddf3794c956d2351540fa92c65b3b)"
+const version = "TaskCollect v1.0.6 (build 7f2ae9fdc464eb8588b8c2bf91311dd1928295d3)"
