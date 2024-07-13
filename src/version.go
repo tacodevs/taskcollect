b2824a9766758ca8394e189df46cfba97d0fd2ac
kvo <kvo@envs.net>
2024-07-13 12:59:51 +0930

    src/server: add date rounding convenience function
    Signed-off-by: kvo <kvo@envs.net>

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const version = "TaskCollect v1.0.6 (build 957cf14b3659bc0296d0b9d986981cea15b8bada)"
+const version = "TaskCollect v1.0.6 (build b757a06434ce01a86636b4b873013058096f7326)"
