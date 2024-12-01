f69031355c1a164379f1eed912b575161ac272f3
kvo <kvo@envs.net>
2024-12-02 09:13:07 +1030

    src/site/daymap: clean up misc constants
    Signed-off-by: kvo <kvo@envs.net>

--- src/version.go
+++ src/version.go
@@ -1,3 +1,3 @@
 package main
 
-const version = "TaskCollect v0.0.0 (build c1ae51f0113ad970022dca827024260ec7079530)"
+const version = "TaskCollect v0.0.0 (build 8d58195006b95b6e05ffb9642fcd222976a1cbd2)"
